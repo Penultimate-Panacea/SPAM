58f30480e0c143be9ff90c49198abec12c2b0ba7
Penultimate-Panacea <rian+github@fantozzi.org>
2018-04-28 18:14:44 -0400

    Continued to add more poetry types

--- core.py
+++ core.py
@@ -28,19 +28,31 @@ def poem_from_file(file_path):
 
 def classify_by_syl(syl_count):
     #TODO: Contine adding poem classifications
+    from numpy import mean
     length = len(syl_count)
-    if length == 5 and syl_count[0] == 2 and syl_count[1] == 4 and syl_count[2] == 6 and syl_count[3] == 8 and syl_count[4] == 2 :
+    if length == 5 and syl_count[0] == 2 and syl_count[1] == 4 and syl_count[2] == 6 and syl_count[3] == 8 and \
+            syl_count[4] == 2 :
         form = "Cinquain"
+    elif length == 9 and syl_count[0] == 9 and syl_count[1] == 8 and syl_count[2] == 7 and syl_count[3] == 6 and \
+            syl_count[4] == 5 and syl_count[5] == 4 and syl_count[6] == 3 and syl_count[7] == 2 and syl_count[8] == 1:
+        form = "Nonet"
+    elif length == 3 and syl_count[0] == 5 and syl_count[1] == 7 and syl_count[2] == 5:
+        form = "Haiku"
+    elif length == 6:
+        form = "Sextain"
+    elif (length == 13 or length == 14) and mean(syl_count) == 8:
+        form = "Formal Rondel"
+    elif 
     else:
-        form = "Freeform"
+        form = "Open Form"
     return form
 poemLines = poem_from_file("This.txt")
 i = 0
 j = 0
-syls = poem[:]
-for i in poem:
+syls = poemLines[:]
+for i in poemLines:
     syls[j] = count_syl(i)
     j += 1
-print(poem)
+print(poemLines)
 print(syls)
 
