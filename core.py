b3d5731d5858f4f8662f877bca3044065d53f4d2
Penultimate-Panacea <rian+github@fantozzi.org>
2018-05-02 03:06:36 -0400

    Reinstated core.py and added progress bar

--- core.py
+++ core.py
@@ -1,2 +1,10 @@
 # encoding='utf-8'
+import importpoem
+import footdetect
+from tqdm import tqdm
+poem = importpoem.poem_from_example("nonet")
+print(poem)
+for line in tqdm(poem):
+    for word in footdetect.linestress("Two households both alike in dignity"):
+        print("\n", footdetect.classicalmeter(word))
 
