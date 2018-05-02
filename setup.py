88f669bfd45125b728749b4e0ca34350ef377d60
Penultimate-Panacea <rian+github@fantozzi.org>
2018-05-02 02:31:56 -0400

    Preemptively added tox for testing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SPAM',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Penultimate Panacea',
+    author_email='',
+    description=''
+)
